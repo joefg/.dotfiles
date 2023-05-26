f5c59772760b34e4809d17a9bdfecd49d5951eea
JFG <joefullergray@gmail.com>
2023-05-26 23:49:47 +0100

    nvim: Abbreviate Q, W to q, q; Wq, WQ to wq

--- nvim/.config/nvim/lua/macros.lua
+++ nvim/.config/nvim/lua/macros.lua
@@ -10,3 +10,9 @@ vim.api.nvim_command('command -range=% Json <line1>,<line2>!python3 -m json.tool
 -- curl
 vim.api.nvim_command('command -range=% Curl <line1>,<line2>!xargs -L 1 curl -s')
 vim.api.nvim_command('command -range=% Curlv <line1>,<line2>!xargs -L 1 curl -v')
+
+-- WQ/Wq/W/Q helper
+vim.api.nvim_command('cabbrev W w')
+vim.api.nvim_command('cabbrev Q q')
+vim.api.nvim_command('cabbrev Wq wq')
+vim.api.nvim_command('cabbrev WQ wq')
