3a7165d205d2ecc0d3fb9a7df2f48d355630b0f7
JFG <joefullergray@gmail.com>
2023-08-19 21:17:58 +0100

    setup: Add install script for nvm and node

---
+++ setup/node-nvm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# nvm is a version manager for node.js
+# for more details, see https://github.com/nvm-sh/nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
