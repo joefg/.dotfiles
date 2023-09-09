6a4c8dc26d29a2f3d780cd1eb0d1253de68ae840
JFG <joefullergray@gmail.com>
2023-09-09 16:14:53 +0100

    nvim: Enable tree view in netrw by default

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -95,6 +95,7 @@ vim.opt.undolevels = 999
 
 -- netrw (vim's file explorer)
 vim.g.netrw_keepdir = 0
+vim.g.netrw_liststyle = 3
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 30
 vim.g.netrw_localcopydircmd = 'cp -r'
