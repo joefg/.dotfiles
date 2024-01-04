c16e89ebe0283580448b3142b246dd1ed0808e79
joefg <joefullergray@gmail.com>
2024-01-04 21:57:19 +0000

    nvim: More statusbar improvements
    * Added mode to statusbar;
    
    * Git status integration uses gitsigns async queries
      for better performance;
    
    * Elements deliminated by unicode arrows;
    
    * Inactive statusbar appears blank;
    
    * General code tidy

--- nvim/.config/nvim/lua/statusline.lua
+++ nvim/.config/nvim/lua/statusline.lua
@@ -5,92 +5,211 @@
 -- /____/\__/\__,_/\__/\__,_/____/_/_/_/ /_/\___/ 
 -- 
 
--- Goal: 
--- mode | git branch | filename | modified | ------- | [ encoding | os ] | ftype | line:col
+local function clean_nils(table)
+	local ret = {}
+	for _, v in pairs(table) do
+		ret[#ret+1] = v
+	end
+	return ret
+end
 
 local M = {}
 
 M.modes = { 
-		['n']     = 'NORMAL',
-		['no']    = 'O-PENDING',
-		['nov']   = 'O-PENDING',
-		['noV']   = 'O-PENDING',
-		['no\22'] = 'O-PENDING',
-		['niI']   = 'NORMAL',
-		['niR']   = 'NORMAL',
-		['niV']   = 'NORMAL',
-		['nt']    = 'NORMAL',
-		['ntT']   = 'NORMAL',
-		['v']     = 'VISUAL',
-		['vs']    = 'VISUAL',
-		['V']     = 'V-LINE',
-		['Vs']    = 'V-LINE',
-		['\22']   = 'V-BLOCK',
-		['\22s']  = 'V-BLOCK',
-		['s']     = 'SELECT',
-		['S']     = 'S-LINE',
-		['\19']   = 'S-BLOCK',
-		['i']     = 'INSERT',
-		['ic']    = 'INSERT',
-		['ix']    = 'INSERT',
-		['R']     = 'REPLACE',
-		['Rc']    = 'REPLACE',
-		['Rx']    = 'REPLACE',
-		['Rv']    = 'V-REPLACE',
-		['Rvc']   = 'V-REPLACE',
-		['Rvx']   = 'V-REPLACE',
-		['c']     = 'COMMAND',
-		['cv']    = 'EX',
-		['ce']    = 'EX',
-		['r']     = 'REPLACE',
-		['rm']    = 'MORE',
-		['r?']    = 'CONFIRM',
-		['!']     = 'SHELL',
-		['t']     = 'TERMINAL',
+	["n"]     = "NORMAL",
+	["no"]    = "O-PENDING",
+	["nov"]   = "O-PENDING",
+	["noV"]   = "O-PENDING",
+	["no\22"] = "O-PENDING",
+	["niI"]   = "NORMAL",
+	["niR"]   = "NORMAL",
+	["niV"]   = "NORMAL",
+	["nt"]    = "NORMAL",
+	["ntT"]   = "NORMAL",
+	["v"]     = "VISUAL",
+	["vs"]    = "VISUAL",
+	["V"]     = "V-LINE",
+	["Vs"]    = "V-LINE",
+	["\22"]   = "V-BLOCK",
+	["\22s"]  = "V-BLOCK",
+	["s"]     = "SELECT",
+	["S"]     = "S-LINE",
+	["\19"]   = "S-BLOCK",
+	["i"]     = "INSERT",
+	["ic"]    = "INSERT",
+	["ix"]    = "INSERT",
+	["R"]     = "REPLACE",
+	["Rc"]    = "REPLACE",
+	["Rx"]    = "REPLACE",
+	["Rv"]    = "V-REPLACE",
+	["Rvc"]   = "V-REPLACE",
+	["Rvx"]   = "V-REPLACE",
+	["c"]     = "COMMAND",
+	["cv"]    = "EX",
+	["ce"]    = "EX",
+	["r"]     = "REPLACE",
+	["rm"]    = "MORE",
+	["r?"]    = "CONFIRM",
+	["!"]     = "SHELL",
+	["t"]     = "TERMINAL"
 }
 
+M.colours = {
+	default       = "",
+	active        = "%#StatusLine#",
+	inactive      = "%#StatuslineNC#",
+	mode          = "%#Mode#",
+	mode_alt      = "%#ModeAlt#",
+	git           = "%#Git#",
+	git_alt       = "%#GitAlt#",
+	filetype      = "%#Filetype#",
+	filetype_al   = "%#FiletypeAlt#",
+	line_col      = "%#LineCol#",
+	line_col_alt  = "%#LineColAlt#",
+	normal        = "%#Normal#"
+}
+
+M.separators = {
+	arrow = {"  ", "  "}, -- If you can't see these arrows, use "blank"
+	blank = {"   ", "   "}  -- instead.
+}
+
+M.sep = function(self, style, direction)
+	if style == nil or style == "blank" then
+		return " "
+	end
+	local sep_style = style
+	local sep_direction = 1
+	if direction == "left"
+		then sep_direction = 1
+	elseif direction == "right"
+		then sep_direction = 2
+	end
+	return M.separators[sep_style][sep_direction]
+end
+
 M.mode = function(self)
-	local currentMode = vim.api.nvim_get_mode().mode
-	if self.modes[currentMode] == nil then
-			return string.format('%s ', currentMode)
+	local current_mode = vim.api.nvim_get_mode().mode
+	if self.modes[current_mode] == nil then
+			return string.format( "%s" , current_mode)
 	end
-	return string.format('%s ', self.modes[currentMode])
-end
-
-M.git_branch = function(self)
-	local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d '\n'")
-	if string.len(branch) > 0 then
-			return string.format('%s ', branch)
-	else
-			return ''
-	end
-end
-
-M.statusline = function(self)
-	local left = {
-		"%#PmenuSel# ",
-		self:git_branch(),
-		"%#LineNr# ",
-		"%f%m "
-	}
-
-	local right = {
-		"[%{&fileencoding?&fileencoding:&encoding} | %{&fileformat}] ",
-		"%y ",
-		"%l:%c "
-	}
 	return string.format(
-		"%s %s %s",
-		table.concat(left, ''),
-		"%=",
-		table.concat(right, '')
+		"%s",
+		self.modes[current_mode]
 	)
 end
 
+M.git_status = function(self, symbol)
+	local git_info = vim.b.gitsigns_status_dict
+	local git_symbol = ""
+	if not git_info or git_info.head == "" then
+		return nil
+	end
+	if symbol then
+		git_symbol = " "
+	end
+	return table.concat({
+		 git_symbol,
+		 git_info.head
+	})
+end
+
+M.lineinfo = function(self)
+	if vim.bo.filetype == "alpha" then
+		return nil
+	end
+	return "%l:%c" 
+end
+
+M.file = function(self)
+	return "%t%m"
+end
+
+M.fileinfo = function(self, separator)
+	local filetype = vim.o.filetype
+	if filetype == "" then
+		filetype = nil
+	end
+	local sep_symbol = " | "
+	if separator ~= nil then
+		sep_symbol = separator
+	end
+	local info = {
+		"%{&fileencoding?&fileencoding:&encoding}",
+		"%{&fileformat}",
+		filetype
+	}
+	return string.format(
+		"%s",
+		table.concat(clean_nils(info), sep_symbol)
+	)
+end
+
+M.component = function(self, text, colour, separator, direction)
+	if text == nil then
+		return nil
+	end
+	if direction == "left" then
+		return string.format(
+			"%s%s%s",
+			colour,
+			text,
+			self:sep(separator, direction)
+	)
+	elseif direction == "right" then
+		return string.format(
+			"%s%s%s",
+			colour,
+			self:sep(separator, direction),
+			text
+		)
+	end
+end
+
+M.statusline_active = function(self)
+	local separator = "arrow"
+	local use_git_symbol = true
+	local left = {
+		self:component(self:mode(), self.colours.default, separator, "left"),
+		self:component(self:git_status(use_git_symbol), self.colours.default, separator, "left"),
+		self:component(self:file(), self.colours.default, separator, "left")
+	}
+	local right = {
+		self:component(self:fileinfo(self:sep(separator, "right")), self.colours.default, separator, "right"),
+		self:component(self:lineinfo(), self.colours.default, separator, "right")
+	}
+	return string.format(
+		"%s %s %s",
+		" " .. table.concat(clean_nils(left)),
+		"%=",
+		table.concat(clean_nils(right)) .. " "
+	)
+end
+
+M.statusline_inactive = function(self)
+	local bar = {
+		self:component(self:file(), self.colours.normal, "blank", "left")
+	}
+	return " " .. table.concat(bar)
+end
+
 M.setup = function(self)
-	vim.o.laststatus = 2
-	vim.o.statusline = self:statusline()
-	vim.opt.showmode = true
+	Statusline = {}
+	Statusline.active = function()
+		return M:statusline_active()
+	end
+	Statusline.inactive = function()
+		return M:statusline_inactive()
+	end
+	vim.opt.showmode = false
+	vim.opt.statusline = Statusline.active()
+	vim.api.nvim_exec([[
+		augroup Statusline
+		au!
+		au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline.active()
+		au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline.inactive()
+		augroup END
+	]], false)
 end
 
 return M
