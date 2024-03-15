f8032170dd61538b683bd9f01f3d92f8de3ab804
joefg <joefullergray@gmail.com>
2024-03-15 17:19:56 +0000

    bash, zsh: Add bun completions to configs

--- bash/.bashrc.d/10-nvm-dvm-bun.bash
+++ bash/.bashrc.d/10-nvm-dvm-bun.bash
@@ -10,3 +10,12 @@ if [ -r "$HOME/.nvm" ]; then
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   alias nvm="unalias nvm; [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"; nvm $@"
 fi
+
+if [ -r "$HOME/.dvm" ]; then
+  # bun completions
+  [ -s "~/.bun/_bun" ] && source "~/.bun/_bun"
+
+  # bun
+  export BUN_INSTALL="$HOME/.bun"
+  export PATH="$BUN_INSTALL/bin:$PATH"
+fi
--- zsh/.zshrc.d/10-nvm-dvm-bun.zsh
+++ zsh/.zshrc.d/10-nvm-dvm-bun.zsh
@@ -8,3 +8,12 @@ if [ -r "$HOME/.nvm" ]; then
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   alias nvm="unalias nvm; [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"; nvm $@"
 fi
+
+if [ -r "$HOME/.dvm" ]; then
+  # bun completions
+  [ -s "/home/jfg/.bun/_bun" ] && source "/home/jfg/.bun/_bun"
+
+  # bun
+  export BUN_INSTALL="$HOME/.bun"
+  export PATH="$BUN_INSTALL/bin:$PATH"
+fi
