d79aacbe226430dd57a1d2f70e55081c94b43118
joefg <joefullergray@gmail.com>
2024-03-25 19:21:18 +0000

    bash: Fix completions for nvm, dvm, bun

--- bash/.bashrc.d/10-nvm-dvm-bun.bash
+++ bash/.bashrc.d/10-nvm-dvm-bun.bash
@@ -1,21 +1,15 @@
-# deno/dvm support
-if [ -r "$HOME/.dvm" ]; then
+if [ -r $HOME/.dvm ]; then
   export DVM_DIR="$HOME/.dvm"
   export PATH="$DVM_DIR/bin:$PATH"
 fi
 
-# node/nvm support
-if [ -r "$HOME/.nvm" ]; then
+if [ -r $HOME/.nvm ]; then
   export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  [ -s "$NVM_DIR"/bash_completion ] && \. "$NVM_DIR"/bash_completion
   alias nvm="unalias nvm; [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"; nvm $@"
 fi
 
-if [ -r "$HOME/.dvm" ]; then
-  # bun completions
-  [ -s "~/.bun/_bun" ] && source "~/.bun/_bun"
-
-  # bun
+if [ -r $HOME/.bun ]; then
   export BUN_INSTALL="$HOME/.bun"
   export PATH="$BUN_INSTALL/bin:$PATH"
 fi
