7e3fd83739c082ca6c1487e8a84119700281bce5
joefg <joefullergray@gmail.com>
2024-06-19 23:14:45 +0100

    nvim: Remove unused selfs
    My lua linter complained about it, so I removed them.

--- nvim/.config/nvim/lua/statusline.lua
+++ nvim/.config/nvim/lua/statusline.lua
@@ -109,11 +109,45 @@ M.git_status = function(self)
 	})
 end
 
-M.lineinfo = function(self)
+M.lineinfo = function()
 	if vim.bo.filetype == "alpha" then
 		return nil
 	end
 	return "%l:%c" 
+end
+
+M.lsp = function()
+  local count = {}
+  local levels = {
+    errors = "Error",
+    warnings = "Warn",
+    info = "Info",
+    hints = "Hint",
+  }
+
+  for k, level in pairs(levels) do
+    count[k] = vim.tbl_count(vim.diagnostic.get(0, { severity = level }))
+  end
+
+  local errors = ""
+  local warnings = ""
+  local hints = ""
+  local info = ""
+
+  if count["errors"] ~= 0 then
+    errors = " %#LspDiagnosticsSignError# " .. count["errors"]
+  end
+  if count["warnings"] ~= 0 then
+    warnings = " %#LspDiagnosticsSignWarning# " .. count["warnings"]
+  end
+  if count["hints"] ~= 0 then
+    hints = " %#LspDiagnosticsSignHint# " .. count["hints"]
+  end
+  if count["info"] ~= 0 then
+    info = " %#LspDiagnosticsSignInformation# " .. count["info"]
+  end
+
+  return errors .. warnings .. hints .. info
 end
 
 M.file = function(self)
