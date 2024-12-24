518a596af191fa3a6519a8d6542978fdb04a0978
joefg <joefullergray@gmail.com>
2024-12-24 13:19:14 +0000

    bash/zsh: Retire nnn git shortcuts
    No longer in use, so retiring.

--- bash/.bashrc.d/06-nnn.bash
+++
@@ -1,8 +0,0 @@
-# nnn
-if [ -r /usr/local/bin/nnn ]; then
-  nnn_plugs='';
-  if [ -r /usr/bin/git ]; then
-    nnn_plugs=$nnn_plugs'g:!|git log --oneline --graph --all;d:!|git diff;s:!|git status -v;';
-  fi;
-  export NNN_PLUG=$nnn_plugs;
-fi
