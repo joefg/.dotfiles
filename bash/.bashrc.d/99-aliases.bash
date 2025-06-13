db83e7f803869b04559a25af6df868ca51f00bfb
joefg <joefullergray@gmail.com>
2025-06-13 19:57:33 +0100

    feature(bash): Add a cheap HTTP server alias
    Very cheap file transfer on the same network,
    with the additional benefit of serving some HTML
    for development purposes.

--- bash/.bashrc.d/99-aliases.bash
+++ bash/.bashrc.d/99-aliases.bash
@@ -30,6 +30,9 @@ alias open="xdg-open"
 # Networking
 alias publicip="dig +short txt ch whoami.cloudflare @1.0.0.1"
 
+# A simple HTTP server
+alias webserv="python3 -m http.server"
+
 # Private aliases
 if [ -r ~/.aliases.bash ]; then
   source ~/.aliases.bash
