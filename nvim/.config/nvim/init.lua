f113f9faf5effb7edf40546be0478422bf5cb303
joefg <joefullergray@gmail.com>
2025-06-18 15:41:20 +0100

    feature(nvim): Remove editorconfig-vim
    As of nvim v0.11, EditorConfig support is built into
    neovim (and vim v9+, incidentally). So removing this
    now-unneeded plugin.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -22,8 +22,6 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup {
-  -- editorconfig
-  { "editorconfig/editorconfig-vim" },
   -- languages
   { "sheerun/vim-polyglot" },
   -- git
