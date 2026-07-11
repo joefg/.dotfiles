c53ed25f5d6aee2961607ada9b8a4cacd594754a
joefg <me@jfg.name>
2026-07-11 17:18:06 +0100

    refactor(nvim): Clean up

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,13 +4,7 @@
 --  / // / / // // /_ _  / // /_/ // /_/ /
 -- /_//_/ /_//_/ \__/(_)/_/ \__,_/ \__,_/
 --
--- My neovim config.
--- For more info on how to configure neovim with lua,
--- see these guides:
---	* https://github.com/nanotee/nvim-lua-guide
---	* https://oroques.dev/notes/neovim-init/
 
--- Colour scheme
 vim.cmd("colorscheme retrobox")
 
 local joefg = require("joefg")
