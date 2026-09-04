57456e161d3a8411218087c0d6fe8410bfc99a8e
joefg <me@jfg.name>
2026-09-04 14:34:41 +0100

    refactor(nvim): Remove netrw from config
    Since superseded by neo-tree.

--- nvim/.config/nvim/lua/joefg/init.lua
+++ nvim/.config/nvim/lua/joefg/init.lua
@@ -1,13 +1,11 @@
 local M = {}
 
 M.macros = require("joefg.macros")
-M.netrw = require("joefg.netrw")
 M.template = require("joefg.template")
 M.settings = require("joefg.settings")
 
 M.setup = function(config)
   M.settings:setup()
-  M.netrw:setup()
   M.macros:setup()
   M.template:setup()
 end
