3a661a8640d90a6a5e6d22aad19283d65dfb4e7e
joefg <me@jfg.name>
2026-09-04 14:33:20 +0100

    refactor(nvim): Wrap external call macros in check
    This check verifies that the binaries used are
    present and accessible to nvim. If they are,
    the macros are available.

--- nvim/.config/nvim/lua/joefg/macros.lua
+++ nvim/.config/nvim/lua/joefg/macros.lua
@@ -1,17 +1,18 @@
 local M = {}
 
 M.setup = function()
-  -- JSON
-  vim.api.nvim_command("command -range=% Json <line1>,<line2>!python3 -m json.tool")
-  vim.api.nvim_command("command -range=% Jsonc <line1>,<line2>!python3 -m json.tool --compact")
-  vim.api.nvim_command("command -range=% Jsonl <line1>,<line2>!python3 -m json.tool --json-lines")
-  vim.api.nvim_command("command -range=% Jsons <line1>,<line2>!python3 -m json.tool --sort-keys")
+  if vim.fn.executable('python') == 1 then
+    vim.api.nvim_command("command -range=% Json <line1>,<line2>!python3 -m json.tool")
+    vim.api.nvim_command("command -range=% Jsonc <line1>,<line2>!python3 -m json.tool --compact")
+    vim.api.nvim_command("command -range=% Jsonl <line1>,<line2>!python3 -m json.tool --json-lines")
+    vim.api.nvim_command("command -range=% Jsons <line1>,<line2>!python3 -m json.tool --sort-keys")
+  end
 
-  -- curl
-  vim.api.nvim_command("command -range=% Curl <line1>,<line2>!xargs -L 1 curl -s")
-  vim.api.nvim_command("command -range=% Curlv <line1>,<line2>!xargs -L 1 curl -v")
+  if vim.fn.executable('curl') == 1 then
+    vim.api.nvim_command("command -range=% Curl <line1>,<line2>!xargs -L 1 curl -s")
+    vim.api.nvim_command("command -range=% Curlv <line1>,<line2>!xargs -L 1 curl -v")
+  end
 
-  -- WQ/Wq/W/Q helper
   vim.api.nvim_command("cabbrev W w")
   vim.api.nvim_command("cabbrev Q q")
   vim.api.nvim_command("cabbrev Wq wq")
